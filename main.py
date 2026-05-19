bbf5ad87070d21cb2f896399d7c8376f1d11b440
joshuafielden43 <279393087+joshuafielden43@users.noreply.github.com>
2026-05-18 19:29:54 -0700

    Improve main.py with better comments

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
-# main.py - Entry point for Housing List Aggregator
+# main.py - Entry point for Housing List Aggregator (v0.6)
 
 import argparse
 from discovery import first_run_discovery
-# TODO: implement full flow with registry, scraper, etc.
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--discover', action='store_true')
+    parser = argparse.ArgumentParser(description='Housing Waitlist Aggregator')
+    parser.add_argument('--discover', action='store_true', help='Run first-run interactive discovery')
     args = parser.parse_args()
+    
     if args.discover:
         first_run_discovery()
     else:
-        print("Normal run - TODO implement")
+        print('Normal run mode - full scraper coming next')
+        # TODO: load registry, scrape, normalize, changelog, output to $OUTPUT_DIR
